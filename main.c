52d8535133d05bbfc66faedc2cd618ba6cdfe945
Benngki <benediktus.setiawan@binus.ac.id>
2022-06-01 18:48:58 +0700

    ok

--- main.c
+++ main.c
@@ -67,63 +67,269 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #define INF 999
-int main()
-{
-    int n,i,j,start;
-    printf("Masukan jumlah node : ");
-    scanf("%d",&n);
-    int G[n][n],tempGraf[n][n],jarak[n],visit[n],temp[n],count;
 
-    printf("Masukkan jarak antar node :\n");
-    printf("  Note! Jika tidak ada jarak masukkan 0!\n");
+void teksTengah(char *judul, char token, float len, char* mode, int new_line){
+    if (mode == "parent" || mode == "run"){
+        int len = (int) (len - strlen(judul) - 1) / 2;
+        if (mode != "run"){
+            for (int j = 0; j < len*2+strlen(judul); j++)
+            {
+                printf("%c", token);
+            }
+            printf("\n");
+        }
+        for (int k = 0; k < len; k++)
+        {
+            printf(" ");
+        }
+        printf("%s", judul);
+        for (int k = 0; k < len; k++)
+        {
+            printf(" ");
+        }        
+        printf("\n");
+        if (mode != "run"){
+            for (int j = 0; j < len*2+strlen(judul); j++)
+            {
+                printf("%c", token);
+            }
+            printf("\n");      
+        }
+    }
+    else if (mode == "pembatas"){
+         len = (int) len;
+        for (int j = 0; j < len-1; j++)
+        {
+            printf("%s", judul);
+        }
+        printf("%c", token);
+        if (new_line){
+            printf("\n");
+        }
+    }
+    else{
+        len = (int) round((len - strlen(judul) - 1) / 2);
+        for (int j = 0; j < len; j++)
+        {
+            printf("%c", token);
+        }
+        printf("%s", judul);
+        for (int k = 0; k < len; k++)
+        {
+            printf("%c", token);
+        }
+        if (new_line){
+            printf("\n");
+        }
+    }
+    
+}
+
+void header(){
+    printf(" "); teksTengah("", '_', 60, "biasa", 1);
+    printf("|"); teksTengah("", ' ', 60, "biasa", 0); printf("|\n");
+    printf("|"); teksTengah("/--------------------------------\\", ' ', 60, "biasa", 0); printf("|\n");
+    printf("|"); teksTengah("| PROGRAM PENCARIAN RUTE TERPENDEK |", ' ', 60, "biasa", 0); printf("|\n");
+    printf("|"); teksTengah("\\--------------------------------/", ' ', 60, "biasa", 0); printf("|\n");
+    printf("|"); teksTengah("-- GRAPH IMPLEMENTATION --", ' ', 60, "biasa", 0); printf("|\n");
+    printf("|"); teksTengah("", '_', 60, "biasa", 0); printf("|\n");
+}
+
+void footer(int jarak_min, int *rute, int n){
+    char temp[20];
+    printf(" "); teksTengah("", '_', 60, "biasa", 1);
+    printf("|"); teksTengah("", ' ', 60, "biasa", 0); printf("|\n");
+    
+    // cetak total jarak minimum
+    sprintf(temp, "Total jarak minimum adalah %d", jarak_min);
+    if (strlen(temp)%2 != 0) strcat(temp, " ");
+    printf("|"); teksTengah(temp, ' ', 60, "biasa", 0); printf("|\n");
+
+    printf("|"); teksTengah("Dengan rute terpendek : ", ' ', 60, "biasa", 0); printf("|\n");
+
+    // cetak rute terpendek
+    char str_rute[50];
+    strcpy(str_rute, "");
+    // printf("%d", rute[n-1]);
+    for (int i = n-1; i >= 0; i--){        
+        if (i != 0) sprintf(temp, "%d -> ", rute[i]);
+        else sprintf(temp, "%d", rute[i]);
+        strcat(str_rute, temp);        
+    }
+    if (strlen(str_rute)%2 != 0) strcat(str_rute, " ");
+    printf("|"); teksTengah(str_rute, ' ', 60, "biasa", 0); printf("|\n");
+    printf("|"); teksTengah("", '_', 60, "biasa", 0); printf("|\n");
+}
+
+void tampilkanGraph(int *graph, int n){
+    char temp[20];
+    int len_num=0;
+    printf(" Represemtasi graf dalam bentuk matriks :\n");
+
+    // cari angka terbesar agar lebar tabel disesuaikan
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
-            // if (i == j) G[i][j] = 0;
-            if (i == j){
-                printf("  %d -> %d = %d\n", i+1, j+1, 0);                
-                G[i][j] = 0;
-                continue;
-            }
-            printf("  %d -> %d = ", i+1, j+1);
-            scanf("%d", &G[i][j]);
+            sprintf(temp, "%d", *(graph + i*n + j) );
+            if (strlen(temp) > len_num) len_num = strlen(temp);
         }
     }
 
-    printf("Masukan node Asal : ");
-    scanf ("%d",&start);
-    start--;
+    int lebar_node = len_num+4, lebar_nomor;
+    sprintf(temp, "%d", n);
+    lebar_nomor = strlen(temp)+3;
+    //  cetak pembatas nomor atas
+    printf("\n ");
+    teksTengah("", ' ', lebar_nomor, "biasa", 0);
+    for (int i = 0; i < n; i++){
+        sprintf(temp, "%d", i+1);
+        printf("");
+        teksTengah(temp, ' ', lebar_node, "biasa", 0);
+    }
+    printf("\n ");
+    teksTengah("_", '|', lebar_nomor, "pembatas", 0);
+    for (int i = 0; i < n; i++){
+        sprintf(temp, "%d", i+1);
+        teksTengah("_", '|', lebar_node, "pembatas", 0);
+    }
+    printf("\n ");
+    
+    // print graf dalam bentuk matriks
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (j == 0) {
+                sprintf(temp, "%d", i+1);
+                teksTengah(temp, ' ', lebar_nomor-1, "biasa", 0); printf("|");
+            }
+            sprintf(temp, "%d", *(graph + i*n + j));
+            teksTengah(temp, ' ', lebar_node, "biasa", 0); 
+            if (strlen(temp) == lebar_node-4) printf("\b");
+            printf("|");
+        }
+        printf("\n ");
+        teksTengah("-", '|', lebar_nomor, "pembatas", 0);
+        for (int i = 0; i < n; i++){
+            sprintf(temp, "%d", i+1);
+            teksTengah("-", '|', lebar_node, "pembatas", 0);
+        }
+        printf("\n ");
+    }    
+}
 
-    for(i=0;i<n;i++)
+void cariRuteTerpendek(int jum_node, int awal, int *rute, int *jarak, int *temp, int *panjang_rute){
+    //nenampilkan jalur dan jarak untuk setiap node
+    for (int i = 0, k, j, z=0; i < jum_node ;i++)
     {
-        for (j=0;j<n;j++)
+        *panjang_rute = 0;
+        z = 0;
+        if(i!=awal)
         {
-            if (G[i][j] == 0)
+            printf ("\n Hasil jarak untuk node ke-%d adalah %d\n",i+1,jarak[i]);
+
+            j=i;
+            printf (" %d <- ", i+1);
+            rute[z] = i+1;
+            z++;
+            *panjang_rute+=1;
+            while(j!=awal)
+            {
+                j=temp[j];
+                printf ("%d",j+1);
+                rute[z] = j+1;
+                z++;
+                *panjang_rute+=1;
+                if(j!=awal)
+                {
+                    printf (" <- ");
+                }
+            }
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int jum_node,awal;
+
+    // cetak header
+    header();
+
+    printf("\n Masukan jumlah node : ");
+    scanf("%d",&jum_node);
+    // int G[jum_node][jum_node],tempGraf[jum_node][jum_node],jarak[jum_node],visit[jum_node],temp[jum_node],count;
+    int *G,tempGraf[jum_node][jum_node],jarak[jum_node],visit[jum_node],temp[jum_node],count;
+    G = (int*) malloc(jum_node*jum_node*sizeof(int));
+
+    printf(" Masukkan jarak antar node :\n");
+    printf("   Note! Jika tidak ada jarak masukkan 0!\n");
+    for (int i = 0; i < jum_node; i++){
+        for (int j = 0; j < jum_node; j++){
+            // opsi 1
+            // if (i == j) G[i][j] = 0;
+
+            // opsi 2
+            // if (i == j){
+            //     printf("   %d -> %d = %d\n", i+1, j+1, 0);                
+            //     G[i][j] = 0;
+            //     continue;
+            // }
+
+            if (j == i) *(G + jum_node*i + j) = 0;
+            else if (j < i) *(G + jum_node*i + j) = *(G + jum_node*j + i);
+            else {
+                printf("   %d -> %d = ", i+1, j+1);
+                scanf("%d", (G + jum_node*i + j));
+                if (j == jum_node-1) printf(" ------------------------------------------------\n");
+            }
+        }        
+    }
+
+    tampilkanGraph(G, jum_node);
+    
+
+    printf("\n Masukan node awal : ");
+    scanf ("%d",&awal);
+    awal--;
+
+    int akhir;
+    printf(" Masukan node akhir : ");
+    scanf ("%d",&akhir);
+    akhir--;
+
+    for(int i=0;i<jum_node;i++)
+    {
+        for (int j=0;j<jum_node;j++)
+        {
+            if (*(G + jum_node*i + j) == 0)
             {
                 tempGraf[i][j] = INF;
             }
             else{
-                tempGraf[i][j] = G[i][j];
+                tempGraf[i][j] = *(G + jum_node*i + j);
             }
         }
     }
-    for (i = 0;i<n;i++)
+    for (int i = 0;i<jum_node;i++)
     {
-        jarak[i] = tempGraf[start][i];
-        temp[i] = start;
+        jarak[i] = tempGraf[awal][i];
+        temp[i] = awal;
         visit[i] = 0;
     }
-    jarak[start] = 0;
-    visit[start] = 1;
+    jarak[awal] = 0;
+    visit[awal] = 1;
 
     count =1; //dimulai dari 1 karena kita tidak akan menghitung node asal lagi
 
     //proses untuk menghitung node yang dikunjungi
     int jarakmin,nextnode;
-    while (count < n-1)
+    // while (count < jum_node-1)
+    while (count < jum_node-1)
     {
         jarakmin = INF;
-        for (i=0;i<n;i++)
+        for (int i=0;i<jum_node;i++)
         {
             //jika jarak lebih kecil dari jarak minimum dan node belum dikunjungi
             // maka jarak minimum adalah jarak yang sudah dibandingkan sebelumnya dengan jarakmin
@@ -136,7 +342,7 @@ int main()
 
         // untuk mengecek node selanjutnya yang terhubung dengan node lain yang memiliki jarak minimum
         visit[nextnode] = 1;
-        for(i = 0;i<n;i++)
+        for(int i = 0;i<jum_node;i++)
         {
             if(visit[i]!=1)
             {
@@ -149,44 +355,13 @@ int main()
         }
     count++;
     }
-    //nenampilkan jalur dan jarak untuk setiap node
-    int a[n+1],k;
-    for (i = 0; i < n ;i++)
-    {
-        if(i!=start)
-        {
-            printf ("\nHasil jarak untuk node ke-%d adalah %d\n",i+1,jarak[i]);
 
-            j=i;
-             printf ("%d<-",i+1);
-            while(j!=start)
-            {
-                j=temp[j];
-                printf ("%d",j+1);
-                if(j!=start)
-                {
-                    printf ("<-");
-                }
+    // proses pencarian rute terpendek
+    int rute[jum_node], panjang_rute;
+    cariRuteTerpendek(jum_node, awal, rute, jarak, temp, &panjang_rute);
+    
+    // cetak footer
+    footer(jarak[jum_node-1], rute, panjang_rute); 
 
-            }
-
-        }
-    }
-    printf ("\nTotal Jaraknya adalah %d\n",jarak[n-1]);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,17 +1,5 @@
-    for (int i = 0; i < jum_kota; i++){
-        // if (i == 0) {
-        //     for (int k = 0; k < jum_kota; k++){
-        //         printf("\t%d", k+1);
-        //     }
-        // }
-        for (int j = 0; j < jum_kota; j++){
-            // if (j == 0) printf("%d| ", i);
-            printf("%d ", data_kota[i][j].nilai);
-
-            // if (data_kota[i][j].nilai == 1){
-            //     printf("|%d", data_kota[i][j].jarak);
-            // }
-            // printf("\t");
-        }
-        printf("\n");
+ 0; i--){
+        if (i != 0) sprintf(temp, "%d ->", rute[i]);
+        else sprintf(temp, "%d", rute[i]);
+        strcat(str_rute, temp);        
     }
